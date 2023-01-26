50e22de50ba8f4fd0ff36d58fefb878cac88684b
ManonC12 <manon-c.coutier@wanadoo.fr>
2023-01-26 11:20:36 +0100

    Tests Plateau

--- src/test/java/org/centrale/tp_coutier_grandjean/PlateauTest.java
+++ src/test/java/org/centrale/tp_coutier_grandjean/PlateauTest.java
@@ -31,95 +31,73 @@ public class PlateauTest {
     
 
     /**
-     * Test of initialisationPlateau method, of class Plateau.
+     * Test de initialisationPlateau, de la classe Plateau.
      */
     @Test
     public void testInitialisationPlateau() {
-        System.out.println("initialisationPlateau");
-        Plateau instance = new Plateau();
-        instance.initialisationPlateau();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Plateau p = new Plateau();
+        int i;
+        int j;
+        p.initialisationPlateau();
+        
+        //Les listes ne sont plus vides
+        assertFalse(p.getListeMarqueurs().isEmpty());
+        assertFalse(p.getListePions().isEmpty());
+        
+        //Les listes ont des tailles 12*4
+        assertEquals(p.getListeMarqueurs().size(),12);
+        assertEquals(p.getListePions().size(),12);
+        for (i=0;i<12;i++){
+            assertEquals(p.getListeMarqueurs().get(i).size(),4);
+            assertEquals(p.getListePions().get(i).size(),4);
+        }
+        
+        //Les listes contiennent des points
+        for (i=0;i<12;i++){
+            for (j=0;j<4;j++){
+                assertEquals(p.getListePions().get(i).get(j),".");
+            }
+        }
     }
 
     /**
-     * Test of nettoyerPlateau method, of class Plateau.
+     * Test de nettoyerPlateau, de la classe Plateau.
      */
     @Test
     public void testNettoyerPlateau() {
-        System.out.println("nettoyerPlateau");
-        Plateau instance = new Plateau();
-        instance.nettoyerPlateau();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        int i;
+        int j;
+  
+        Plateau p1 = new Plateau();
+        Plateau p2 = new Plateau();
+        p1.initialisationPlateau();
+        
+        for (i=0;i<20;i++){
+            for (j=0;j<6;j++){
+                p2.getListePions().get(i).set(j, "A");
+                p2.getListeMarqueurs().get(i).set(j, "A");
+            }
+        
+        p2.nettoyerPlateau();
+            
+        //les listes de p2 ont des tailles 12*4
+        assertEquals(p2.getListeMarqueurs().size(),12);
+        assertEquals(p2.getListePions().size(),12);
+        for (i=0;i<12;i++){
+            assertEquals(p2.getListeMarqueurs().get(i).size(),4);
+            assertEquals(p2.getListePions().get(i).size(),4);
+        }
+        
+        //les listes de p2 sont constitués de points
+        for (i=0;i<12;i++){
+            for (j=0;j<4;j++){
+                assertEquals(p1.getListePions().get(i).get(j),p2.getListePions().get(i).get(j));
+                assertEquals(p1.getListeMarqueurs().get(i).get(j),p2.getListeMarqueurs().get(i).get(j));
+            }
+            
+        }
     }
 
-    /**
-     * Test of affichePlateau method, of class Plateau.
-     */
-    @Test
-    public void testAffichePlateau() {
-        System.out.println("affichePlateau");
-        Plateau instance = new Plateau();
-        instance.affichePlateau();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of choixPions method, of class Plateau.
-     */
-    @Test
-    public void testChoixPions() {
-        System.out.println("choixPions");
-        Scanner scan = null;
-        Plateau instance = new Plateau();
-        List<String> expResult = null;
-        List<String> result = instance.choixPions(scan);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of choixCode method, of class Plateau.
-     */
-    @Test
-    public void testChoixCode() {
-        System.out.println("choixCode");
-        Scanner scan = null;
-        Plateau instance = new Plateau();
-        instance.choixCode(scan);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of choixCombinaison method, of class Plateau.
-     */
-    @Test
-    public void testChoixCombinaison() {
-        System.out.println("choixCombinaison");
-        Scanner scan = null;
-        Plateau instance = new Plateau();
-        instance.choixCombinaison(scan);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of verifCombinaison method, of class Plateau.
-     */
-    @Test
-    public void testVerifCombinaison() {
-        System.out.println("verifCombinaison");
-        Scanner scan = null;
-        Plateau instance = new Plateau();
-        boolean expResult = false;
-        boolean result = instance.verifCombinaison(scan);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+    }  
 }
